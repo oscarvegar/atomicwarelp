40a7c98068cbf8593c6dcd0899a401ef63855f36
oscar.vega <oscar.vega@atomicware.mx>
2015-05-06 16:42:16 -0500

    cambios mongo y angular

---
+++ js/atomicware.js
@@ -0,0 +1,15 @@
+angular.module('AtomicwareApp', [])
+.controller('ContactoCtrl', function($scope,$http){
+	$scope.contacto = {};
+	$scope.enviar = function(){
+		console.log($scope.contacto);
+		$http.post("http://atomicware.mx:1337/contacto",$scope.contacto)
+		.success(function(res){
+			console.info(res);
+			alert("Gracias, en breve nos pondremos en contacto contigo.")
+		}).error(function(err){
+			alert("Ocurrió un error intenta de nuevo.")
+		});
+
+	}
+});  
